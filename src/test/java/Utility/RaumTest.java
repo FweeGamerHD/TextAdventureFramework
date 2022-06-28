f0681e87c7bf80aa14c9f9c053ff75f61de0e532
FweeGamerHD <freegamerhd12@gmail.com>
2022-06-28 08:48:47 +0200

    Added some Junit tests (currently empty)

---
+++ src/test/java/Utility/RaumTest.java
@@ -0,0 +1,34 @@
+package Utility;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaumTest {
+
+    @org.junit.jupiter.api.Test
+    void addItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addConnection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getConnection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getConnections() {
+    }
+}
